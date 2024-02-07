e90b3e1943aa045880abf5fde94ae62e267e4d0e
Masaki-Saito84 <saito@hyge.co.jp>
2024-02-07 21:47:33 +0900

    wip: indexページの作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,29 @@
 <script setup lang="ts">
+const cityList = [
+  { name: "tokyo", label: "東京" },
+  { name: "osaka", label: "大阪" },
+  { name: "nagoya", label: "名古屋" },
+]
 </script>
 
 <template>
-  <h1>test</h1>
+  <main>
+    <section class="home__section" v-if="cityList">
+      <h2>主な都市を詳しく見る</h2>
+      <nav>
+        <ul class="city-list">
+          <li class="city-list__item" v-for="(city, index) in cityList" :key="index">
+            <NuxtLink :to="`/${city.name}`">{{ city.label }}</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </section>
+    <section class="home__section">
+      <h2>都市の天気を検索</h2>
+      <form action="" class="city-search">
+        <input type="text" class="city-search__input" placeholder="都市名を入力" />
+        <button type="submit" class="city-search__btn">検索</button>
+      </form>
+    </section>
+  </main>
 </template>
