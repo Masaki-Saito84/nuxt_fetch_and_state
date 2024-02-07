76e788f813631c4c258e4b59006ff5bf0119da56
Masaki-Saito84 <saito@hyge.co.jp>
2024-02-07 21:45:22 +0900

    - 天気情報のstate配列に、エラーの情報が入るようにする - state追加時、重複する地域の情報はstateから削除する - 取得から10分経過した場合、stateに情報を追加する

--- composables/use_weather.ts
+++ composables/use_weather.ts
@@ -1,19 +1,30 @@
-import type { WeatherData } from "~/interfaces/WeatherData"
+import type { WeatherData, FetchWeatherError } from "~/interfaces/WeatherData"
 
 export const useWeather = () => {
-  const weatherList: Ref<Array<WeatherData>> = useState<Array<WeatherData>>("weatherList", () => [])
+  const weatherList: Ref<Array<WeatherData | FetchWeatherError>> = useState<Array<WeatherData>>("weatherList", () => [])
 
-  const addWeatherData = async(city: string) => {
-    const baseList = weatherList.value
-    const {data} = await $fetch(`/api/weather/${city}`)
-    if (data.cod === 200) {
-      baseList.push(data)
+  const updateWeatherList = async (city: string) => {
+
+    const baseList = weatherList.value.filter((data) => data.city_param !== city)
+    const { data, error } = await $fetch(`/api/weather/${city}`) as { data: WeatherData, error: FetchWeatherError}
+
+      baseList.push(data ? data : error)
       weatherList.value = baseList
+  }
+
+  const getWeatherData = () => {
+    const { name: routeName, params: routeParams } = useRoute()
+    const city = routeName === 'index' ? 'tokyo' : routeParams.city
+    const weatherData = weatherList.value.find((data) => data.city_param === city)
+
+    if (weatherData && new Date(weatherData.fetch_date).getTime() > new Date().getTime() - 600000) {
+      return weatherData
     }
+    updateWeatherList(city.toString())
   }
 
   return {
     weatherList,
-    addWeatherData,
+    getWeatherData,
   }
 }
